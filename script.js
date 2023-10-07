10a26cded675eb468960a9e63902735e73f4789e
mherrera491 <mattherrera1@gmail.com>
2023-10-07 09:55:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// GAME CHOICE: CONCENTRATION GAME (MEMORY GAME)
+// Create a deck of cards (even number amount)
+// Shuffle cards
+// create game board with cards
+// add event listeners to card to flip it
+// If second card matches the first card then remove cards from the game board
+// If card does not match the previous card then flip cards back over and let player know they are wrong
+// Create a timer for the game - if player runs out of time they lose
+// If cards are all cleared announce that player has won
+
+
+
+
+// Blackjack
+
+// create a deck of cards... an array with 52 objects
+// create shuffle function
+// Create a player hand and a dealer hand
+// Deal two cards to each player and the dealer
+// If either player or dealer get 21 after first deal they win
+// Prompt the player to hit or stand
+// If the player hits, deal them another card
+// If the player stands, calculate their hand total
+// If the player's hand total is 21, they win
+// If the player's hand total is over 21, they lose
+// If the player's hand total is less than 21, the dealer draws cards until their hand total is greater than 17
+// If the dealer's hand total is over 21, they lose
+// If the dealer's hand total is less than or equal to 21, the dealer wins
+// Declare a winner
+// Add wager feature - if player wins, they win the wager. If player loses, they lose the wager
+// If dealer's hand and player's hand equals each other then it results in a push and player gets their wager back
